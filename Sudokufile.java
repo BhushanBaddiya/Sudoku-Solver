a7daf0601c04e666000d1bc071f6782bde01bcae
BhushanBaddiya <bhushanbaddiya0@gmail.com>
2023-10-14 13:13:20 +0530

    update

--- Sudokufile.java
+++ Sudokufile.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.mycompany.sudoku_solver;
+
 
 import static java.awt.Color.black;
 import static java.awt.Color.orange;
